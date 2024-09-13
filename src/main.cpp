86b7af00ccef7de265ad81646e102a2faaf6873f
kerichdev <kerichuudev@gmail.com>
2024-09-14 00:11:45 +0300

    feat: add ascii and reference cli tools

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include "lib/common/cli.h"
 
 int main () {
-    std::cout << "hello from desktop provisioning" << std::endl;
+    cli::log("Hello from desktop provisioning!");
+
+    std::cout << "        -*+-                                                              " << std::endl;
+    std::cout << "     =*######*=                                                           " << std::endl;
+    std::cout << "  +*#############+                                                        " << std::endl;
+    std::cout << "  *##############*.       :-.          #%.                                " << std::endl;
+    std::cout << "   =####+  *####+      =@@@%@@%.       %@.                                " << std::endl;
+    std::cout << "    :*#:    -##:      *@#     @@. =@@@@@@.-@%  -@% %@@@@  .%@@@%.  @#@@@%." << std::endl;
+    std::cout << "     ..      .        #@.     *@--@#   @@. =@+ @% :@%=:  :@@---@@. @%  :@#" << std::endl;
+    std::cout << "    :*#:    -##.      -@@:   +@@ -@#   @@.  %@@@.   .-%@=:@@:..:.  @#   @#" << std::endl;
+    std::cout << "   =####+  *####=       *@@@@%=   =@@@@#@.   @@:  :@@@@%  .%@@@@=  @#   @#" << std::endl;
+    std::cout << "  *##############*                          +@*                           " << std::endl;
+    std::cout << "  +##############+                          #+                            " << std::endl;
+    std::cout << "     =*######*=                                                           " << std::endl;
+    std::cout << "        -**-                                                              " << std::endl;
+
+    cli::log("...");
 }
