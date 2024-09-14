5da84a61ab30c9ffc17b9d85bda653259c792f5b
kerichdev <kerichuudev@gmail.com>
2024-09-14 23:28:50 +0300

    feat: add base defines for use

---
+++ src/lib/common/base.h
@@ -0,0 +1,11 @@
+#define nulloutp " > /dev/null"
+#define logoutps " > ./provisioner.log"
+#define logoutp " >> ./provisioner.log"
+
+#define red "\033[31m"
+#define ylw "\033[33m"
+#define grn "\033[32m"
+
+#define def "\033[0m"
+#define inv "\033[7m"
+#define unv "\033[27m"
