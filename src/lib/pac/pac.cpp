828d79f6a90df4d9d6b71e127938934ed95ef63f
kerichdev <kerichuudev@gmail.com>
2024-09-15 23:06:30 +0300

    chore: cleanups and prep for new impl

--- src/lib/pac/pac.cpp
+++ src/lib/pac/pac.cpp
@@ -7,19 +7,17 @@
 #include "lib/common/cli.h"
 #include "lib/common/utils.h"
 
-void pac::strap::install(std::string kerned) // TODO: rewrite the pacstrap implementation to use a package list pre-supplied in the config
+void pac::strap::install(std::string packageListPath) // TODO: rewrite the pacstrap implementation to use a package list pre-supplied in the config
 {
-	std::cout << ylw << "\nInstalling basic packages...\n" << def;
 	cli::log("Installing basic packages...");
 
-	std::string kern = (kerned == "mainline") ? "linux" : ("linux-" + kerned);
-	std::string pacs = "base " + kern + " linux-firmware";
+	//std::string kern = (kerned == "mainline") ? "linux" : ("linux-" + kerned);
+	//std::string pacs = "base " + kern + " linux-firmware";
 	
-	std::string toinstall_s = "pacstrap /mnt --noconfirm " + pacs;
+	std::string toinstall_s = "pacstrap /mnt --noconfirm ";
 
 	system(toinstall_s.c_str());					//	< ---------------------------------------
 	
-	std::cout << grn << "Done.\n" << def;
 	cli::log("Done.");
 }
 
@@ -27,7 +25,6 @@ void pac::strap::install(std::string kerned) // TODO: rewrite the pacstrap imple
 
 void pac::strap::setmirr(std::string CC)
 {
-	std::cout << ylw << "\nReaching national mirror...\n" << def;
 	cli::log("Reaching national mirror...");
 		
 	std::string toreach_s = "reflector -c \"" + CC + "\" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist";
@@ -35,24 +32,20 @@ void pac::strap::setmirr(std::string CC)
 	system("cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak");		//	< ---------------------------------------
 	system(toreach_s.c_str());					//	< ---------------------------------------
 	
-	std::cout << grn << "Done.\n" def;
 	cli::log("Done.");
 }
 
 void pac::man::refresh()
 {
-	std::cout << ylw << "\nSyncing packages\' repositories...\n" << def;
 	cli::log("Syncing packages repositories...");
 
 	system("pacman -Syy");					//	< ---------------------------------------
 	
-	std::cout << grn << "Done.\n" << def;
 	cli::log("Done.");
 }
 
-void pac::man::install(std::string* pkgs)
+void pac::man::install(std::string* pkgs) // TODO: deprecate if pacstrap installs all the neccessary packages
 {
-	std::cout << ylw << "\nInstalling utilities...\n" << def;
 	cli::log("Installing utilities...");
 
 	std::string pacs;
@@ -65,6 +58,5 @@ void pac::man::install(std::string* pkgs)
 
 	system(toinstall_s.c_str());					//	< ---------------------------------------
 	
-	std::cout << grn << "Done.\n" << def;
 	cli::log("Done.");
 }
