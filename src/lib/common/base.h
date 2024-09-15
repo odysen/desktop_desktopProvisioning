dfa7492605c0401fbd502e14c75afce7633bd35b
kerichdev <kerichuudev@gmail.com>
2024-09-15 22:58:19 +0300

    chore: document bash formatting tags

--- src/lib/common/base.h
+++ src/lib/common/base.h
@@ -1,7 +1,7 @@
-#define red "\033[31m"
-#define ylw "\033[33m"
-#define grn "\033[32m"
+#define red "\033[31m" //red
+#define ylw "\033[33m" //yellow
+#define grn "\033[32m" //green
 
-#define def "\033[0m"
-#define inv "\033[7m"
-#define unv "\033[27m"
+#define def "\033[0m" // default
+#define bld "\033[1m" // bold
+#define inv "\033[7m" // inverse
