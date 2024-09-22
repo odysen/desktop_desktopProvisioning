7cf77f56255a40e9c0507eafaaadd0aa3fc1d61f
kerichdev <kerichuudev@gmail.com>
2024-09-22 12:34:48 +0300

    feat: provisioner argument handler

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,23 @@
 #include "lib/partitioner/partitioner.h"
 #include "lib/userspace/userspace.h"
 
+#include "lib/provisioning/provisioning.h"
+
 int main (int argc, char* argv[]) {
     cli::log("Hello from desktop provisioning!");
 
+    if (argc > 1) {
+        if (std::string(argv[1]) == "provision" && argc < 3) {
+            cli::error("Running in GUI provisioning mode, but no command was provided.");
+            return 1;
+        }
+
+        else {
+            cli::log("Passing to provisioning...");
+            prov::execute(argv[3]);
+            return 0;
+        } // TODO: need to handle incorrect commands
+    }
     std::cout << "        -*+-                                                              " << std::endl;
     std::cout << "     =*######*=                                                           " << std::endl;
     std::cout << "  +*#############+                                                        " << std::endl;
