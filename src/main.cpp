de8f481736086e79edf1011d04ad7cc6bb163699
kerichdev <kerichuudev@gmail.com>
2024-09-29 11:06:39 +0300

    feat: implement connectivity and root checks (dirty)

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unistd.h>
 #include "lib/common/cli.h"
 
 #include "lib/pac/pac.h"
@@ -9,6 +10,24 @@
 
 int main (int argc, char* argv[]) {
     cli::log("Hello from desktop provisioning!");
+
+    cli::log("Ensure an internet connection is available before proceeding! You can do so with the GUI of Odysen, in the settings, etc.");
+
+        // still ensure whether we're connected
+    if (system("ping -c1 pkg.odysen.space")) {
+        cli::error("No internet connection detected. Exiting.");
+        exit(1);
+    } else {
+        cli::okay("Internet connection detected.");
+    }
+
+    // now we check if we're root
+    if (getuid()) {
+        cli::error("Not running as root. Exiting.");
+        exit(1);
+    } else {
+        cli::okay("Running as root.");
+    }
 
     std::cout << "        -*+-                                                              " << std::endl;
     std::cout << "     =*######*=                                                           " << std::endl;
@@ -27,5 +46,4 @@ int main (int argc, char* argv[]) {
     cli::log("Running in CLI mode...");
 
     // now we go as per arch wiki
-    cli::log("Ensure an internet connection is available before proceeding!");
 }
