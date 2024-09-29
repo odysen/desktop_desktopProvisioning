72546334e077eff0dbf46cd1072a2a1ca94a96ee
kerichdev <kerichuudev@gmail.com>
2024-09-29 10:56:05 +0300

    refactor: ditch gui-provisioning for now

--- src/main.cpp
+++ src/main.cpp
@@ -10,19 +10,6 @@
 int main (int argc, char* argv[]) {
     cli::log("Hello from desktop provisioning!");
 
-    if (argc > 1) {
-        if (std::string(argv[1]) == "provision" && argc < 3) {
-            cli::error("Running in GUI provisioning mode, but no command was provided.");
-            return 1;
-        }
-
-        else {
-            cli::log("Passing to provisioning...");
-            prov::execute(argv[3]);
-            return 0;
-        } // TODO: need to handle incorrect commands
-        // speaking of -- the provisioner will need to handle internet connection somehow, hmm...
-    }
     std::cout << "        -*+-                                                              " << std::endl;
     std::cout << "     =*######*=                                                           " << std::endl;
     std::cout << "  +*#############+                                                        " << std::endl;
